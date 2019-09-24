8ec32306b27cf1900c94c377ac67245e0cbcb6a1
Gerard Bentley <gbkh2015@pop-os.localdomain>
2019-09-24 13:18:26 -0700

    example json, tagged channel processing

--- vgac_tagging/db.py
+++ vgac_tagging/db.py
@@ -1,11 +1,10 @@
 import os
 import glob
 
-import numpy as np
 ''' Lists all png images with file structure DIR/GAME_NAME/img/0.png'''
 
 
-def get_image_files(dir=os.path.join('..', '..', 'affordances_corpus', 'games')):
+def get_image_files(dir=os.path.join('..', 'affordances_corpus', 'games')):
     image_files = []
     for game in list_games(dir):
         per_game_files = glob.glob(os.path.join(dir, game, 'img', '*.png'))
@@ -13,20 +12,6 @@ def get_image_files(dir=os.path.join('..', '..', 'affordances_corpus', 'games'))
     return image_files
 
 
-def try_get_label(image_file=os.path.join('..', '..', 'affordances_corpus', 'games', 'loz', 'img', '0.png')):
-    label_file = image_file.replace('img', 'label').replace('png', 'npy')
-    if os.path.isfile(label_file):
-        print('Label File Found')
-        match_all_old = False
-        stacked_tensor = np.load(label_file)
-    else:
-        print('New Label File')
-        match_all_old = True
-        stacked_tensor = np.full(
-            [224, 256, 9], fill_value=0.5)
-    return stacked_tensor
-
-
-def list_games(dir=os.path.join('..', '..', 'affordances_corpus', 'games')):
+def list_games(dir=os.path.join('..', 'affordances_corpus', 'games')):
     games = next(os.walk(dir))[1]
     return games
