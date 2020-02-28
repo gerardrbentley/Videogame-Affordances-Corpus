88478c639eadde71b801b1d15773f3782bc6f5d3
Gerard Bentley <gbkh2015@mymail.pomona.edu>
2020-02-28 14:44:00 -0800

    texture matching 8x8s sqdiff or ccorr

--- texture_matching/texture_matching.py
+++ texture_matching/texture_matching.py
@@ -51,7 +51,7 @@ def main(args):
     cv2.imwrite(f"{name}_orig.png", orig_image)
 
 def load_known_templates(dir='./textures', fill_color=[0,0,0,255]):
-    """Loads textures from pngs in directory. For textures with alpha fills with fill_color.
+    """Loads textures from pngs in directory. For textures with empty alpha areas, crops to smallest size without losing pixels and fills with fill_color.
     Yields dictionary mapping filenames to filled_texture, binary alpha mask, and original texture in 4 channel
     
     Keyword Arguments:
@@ -59,41 +59,62 @@ def load_known_templates(dir='./textures', fill_color=[0,0,0,255]):
         fill_color {list} -- Fill color for alpha channel (default: {[0,0,0,255]})
     
     Returns:
-        dict[str] -> (filled_img_bgr, binary_mask, orig_img)
+        dict[str] -> (filled_img_bgr, binary_mask, orig_img_bgra)
     """    
     known_templates = {}
+    total = 0
+    alpha_ct = 0
     for file in glob.glob(os.path.join(dir, '*.png')):
+        total += 1
         file_name = os.path.split(file)[1]
         name = os.path.splitext(file_name)[0]
         img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
         channels = cv2.split(img)
-        if(len(channels) == 1):
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
-            channels = cv2.split(img)
-        if(len(channels) == 3):
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
-            channels = cv2.split(img)
+  
+
+        if len(channels) == 1:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         
-        #coords of nonzero alpha values
-        y,x = img[:,:,3].nonzero()
-        crop_x1 = np.min(x)
-        crop_y1 = np.min(y)
-        crop_x2 = np.max(x)
-        crop_y2 = np.max(y)
+        all_alpha = False
+        if len(channels) == 4 :
+            if np.count_nonzero(img[:,:,3]) == 0:
+                all_alpha = True
+                alpha_ct += 1
+                continue
+            #coords of nonzero alpha values
+            y,x = img[:,:,3].nonzero()
+            crop_y1 = np.min(y)
+            crop_x1 = np.min(x)
+            crop_x2 = np.max(x)
+            crop_y2 = np.max(y)
 
-        cropped_tex = np.copy(img)[crop_y1:crop_y2, crop_x1:crop_x2]
-        channels = cv2.split(cropped_tex)
-        # Used for painting image only where template has data
-        mask = np.array(channels[3])
-        mask[channels[3] == 0] = 0
-        mask[channels[3] == 255] = 255
+            # cropped_tex = np.copy(img)[crop_y1:crop_y2+1, crop_x1:crop_x2+1]
+            
+            # channels = cv2.split(cropped_tex)
+            
+            # Used for painting image only where template has data
+            mask = np.array(channels[3])
+            mask[channels[3] == 0] = 0
+            mask[channels[3] == 255] = 255
 
-        # Add background color to template match with mask but punish background color
-        out_image = np.copy(cropped_tex)
-        out_image[channels[3] == 0] = fill_color
-        out_image = cv2.cvtColor(out_image, cv2.COLOR_BGRA2BGR)
+            # Add background color to template match with mask but punish background color
+            out_image = np.copy(img)
+            out_image[channels[3] == 0] = fill_color
+            out_image = cv2.cvtColor(out_image, cv2.COLOR_BGRA2BGR)
+        else:
+            out_image = np.copy(img)
+            mask = np.full_like(img[:,:,0], 255)
 
-        known_templates[name] = (out_image, mask, img)
+        flag = False
+        for key, data in known_templates.items():
+            old_out = data[0]
+            if np.array_equal(old_out, out_image):
+                flag = True
+                break
+            
+        if not flag and not all_alpha:
+            known_templates[name] = (out_image, mask, img)
+    print(f"Total texs loaded: {total}. Minimized: {len(known_templates)}, all alpha: {alpha_ct}")
     return known_templates
 
 def match_known_textures(image, known_textures, args):
@@ -112,22 +133,36 @@ def match_known_textures(image, known_textures, args):
     """    
     dupe_ctr = 0
     ctr = 0
-    colorset = get_colorset(100)
+    colorset = get_colorset(300)
 
     unmatched = []
     matched_textures = []
     matched_locations = []
-    out_image = np.zeros_like(image, np.uint8)
-    confidence_map = np.zeros((image.shape[0], image.shape[1]), np.float)
+    out_image = np.ones_like(image, np.uint8)
+    if args.sqdiff:
+        confidence_map = np.ones((image.shape[0], image.shape[1]), np.float)
+        comparison = lambda x,y: x < y
+        print('Compare using less than')
+    else:
+        confidence_map = np.zeros((image.shape[0], image.shape[1]), np.float)
+        comparison = lambda x,y: x > y
     for file_name, data in known_textures.items():
         old_texture, mask, orig = data
-        results = cv2.matchTemplate(image, old_texture, cv2.TM_CCORR_NORMED)
-        results = np.where(~np.isnan(results), results, 0)
-        results = np.where(~np.isinf(results), results, 0)
-        loc = np.where(results >= args.threshold)
-        # minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(results)
-
+        if args.sqdiff:
+            results = cv2.matchTemplate(image, old_texture, cv2.TM_SQDIFF_NORMED)
+            results = np.where(~np.isnan(results), results, 0)
+            results = np.where(~np.isinf(results), results, 0)
+            loc = np.where(results <= args.threshold)
+        else:
+            results = cv2.matchTemplate(image, old_texture, cv2.TM_CCORR_NORMED)
+            results = np.where(~np.isnan(results), results, 0)
+            results = np.where(~np.isinf(results), results, 0)
+            loc = np.where(results >= args.threshold)
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(results)
         matches = list(zip(*loc[::1]))
+        if file_name == 'tile_199':
+            print(f"00: {minVal}, {maxVal}, {minLoc}, {maxLoc}")
+            print(len(matches))
         if len(matches) > 9000:
             print(f"OVER 9000 {file_name}: {len(matches)}")
         if len(matches) != 0 and len(matches) < 9000:
@@ -152,7 +187,7 @@ def match_known_textures(image, known_textures, args):
                 new_confidence = results[match_row, match_col]
                 for (r, c) in indices:
                     old_confidence = confidence_map[r, c]
-                    if mask[r, c] == 255 and new_confidence > old_confidence:
+                    if mask[r, c] == 255:
                         if old_confidence != 0.0:
                             dupe_ctr += 1
                         out_image[match_row+r,match_col+c, :] = [curr_color[0]*255, curr_color[1]*255, curr_color[2]*255]
@@ -160,7 +195,8 @@ def match_known_textures(image, known_textures, args):
         else:
             unmatched.append(file_name)
     if args.verbose:
-        print(matched_textures)
+        
+        print(sorted(matched_textures))
         print(len(unmatched))
         print(len(matched_locations))
         print(len(matched_textures))
@@ -188,6 +224,8 @@ def parse_args():
     parser.add_argument('--threshold', type=float, default=0.985)
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--save-imgs', action='store_true')
+    parser.add_argument('--save-np', action='store_true')
+    parser.add_argument('--sqdiff', action='store_true')
 
     args = parser.parse_args()
 
